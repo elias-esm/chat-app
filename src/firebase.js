5d6a111c62635b690bdbee1aa103f90a230ae515
Elias <elsesmaeili@gmail.com>
2023-06-19 01:28:59 +0330

    botogram chat app

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+export const auth = firebase.initializeApp({
+  apiKey: "AIzaSyCo0F3wQcfSYg6DLIDv_itROkp6X4_rKFE",
+  authDomain: "botogram-e93a7.firebaseapp.com",
+  projectId: "botogram-e93a7",
+  storageBucket: "botogram-e93a7.appspot.com",
+  messagingSenderId: "118680594650",
+  appId: "1:118680594650:web:33014e77d4909656d3dde3"
+}).auth();
+
+
+
+  
